113cb57c79b6b884fe2915359796764a29fcd120
Edwin <edwin.h.morris@gmail.com>
2014-05-05 21:39:23 -0400

    add total duration

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,8 +1,26 @@
 class PlacesController < ApplicationController
   def show
-    places_count = Place.group(:name).count
-    places_sorted = places_count.sort_by{ |place, visits| visits }.reverse!
+    @places_and_visited_count = places_and_visited_count
+  end
 
-    @places_and_visited_count = places_sorted
+  private
+
+  def places_and_visited_count
+    places = Place.group(:name).count
+    places_sorted = places.sort_by{ |place, visits| visits }.reverse!
+
+    places_sorted.each do |place|
+      place[2] = total_time_spent_in_place(place[0])
+    end
+  end
+
+  def total_time_spent_in_place place_name
+    total_duration = 0
+
+    Place.where(name: place_name).each do |place|
+      total_duration += place.duration.to_i
+    end
+
+    total_duration
   end
 end
